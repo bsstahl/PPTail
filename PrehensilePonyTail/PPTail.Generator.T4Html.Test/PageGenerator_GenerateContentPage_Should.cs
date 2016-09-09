0fad6e77b36112d2d2bc4052cff1edde1ec5793f
Barry Stahl <barry@bsstahl.com>
2016-09-09 15:51:38 -0700

    Add tests RemoveThePlaceholderTextIfTheTitleDataValueIsNull, RemoveThePlaceholderTextIfTheContentDataValueIsNull, ReplaceAllAuthorPlaceholdersWithTheAuthor and RemoveThePlaceholderTextIfTheAuthorDataValueIsNull.

--- PrehensilePonyTail/PPTail.Generator.T4Html.Test/PageGenerator_GenerateContentPage_Should.cs
+++ PrehensilePonyTail/PPTail.Generator.T4Html.Test/PageGenerator_GenerateContentPage_Should.cs
@@ -39,6 +39,24 @@ namespace PPTail.Generator.T4Html.Test
         }
 
         [Fact]
+        public void RemoveThePlaceholderTextIfTheTitleDataValueIsNull()
+        {
+            const string placeholderText = "{Title}";
+
+            var pageData = (null as ContentItem).Create();
+            pageData.Title = null;
+
+            string template = $"{placeholderText}*******{placeholderText}******\r\n****{placeholderText}*********\t\t****{placeholderText}*****{placeholderText}************{placeholderText}";
+            var target = (null as IPageGenerator).Create(template, string.Empty);
+
+            var actual = target.GenerateContentPage(pageData);
+            Console.WriteLine(actual);
+
+            int actualCount = actual.Select((c, i) => actual.Substring(i)).Count(sub => sub.StartsWith(placeholderText));
+            Assert.Equal(0, actualCount);
+        }
+
+        [Fact]
         public void ReplaceAContentPlaceholderWithTheContent()
         {
             const string placeholderText = "{Content}";
@@ -70,6 +88,60 @@ namespace PPTail.Generator.T4Html.Test
 
             int actualCount = actual.Select((c, i) => actual.Substring(i)).Count(sub => sub.StartsWith(expectedData));
             Assert.Equal(6, actualCount);
+        }
+
+        [Fact]
+        public void RemoveThePlaceholderTextIfTheContentDataValueIsNull()
+        {
+            const string placeholderText = "{Content}";
+
+            var pageData = (null as ContentItem).Create();
+            pageData.Content = null;
+
+            string template = $"{placeholderText}*******{placeholderText}******\r\n****{placeholderText}*********\t\t****{placeholderText}*****{placeholderText}************{placeholderText}";
+            var target = (null as IPageGenerator).Create(template, string.Empty);
+
+            var actual = target.GenerateContentPage(pageData);
+            Console.WriteLine(actual);
+
+            int actualCount = actual.Select((c, i) => actual.Substring(i)).Count(sub => sub.StartsWith(placeholderText));
+            Assert.Equal(0, actualCount);
+        }
+
+        [Fact]
+        public void ReplaceAllAuthorPlaceholdersWithTheAuthor()
+        {
+            const string placeholderText = "{Author}";
+
+            var pageData = (null as ContentItem).Create();
+            var expectedData = pageData.Author;
+
+            string template = $"{placeholderText}*******{placeholderText}******\r\n****{placeholderText}*********\t\t****{placeholderText}*****{placeholderText}************{placeholderText}";
+            var target = (null as IPageGenerator).Create(template, string.Empty);
+
+            var actual = target.GenerateContentPage(pageData);
+            Console.WriteLine(actual);
+
+            int actualCount = actual.Select((c, i) => actual.Substring(i)).Count(sub => sub.StartsWith(expectedData));
+            Assert.Equal(6, actualCount);
+        }
+
+        [Fact]
+        public void RemoveThePlaceholderTextIfTheAuthorDataValueIsNull()
+        {
+            const string placeholderText = "{Author}";
+
+            var pageData = (null as ContentItem).Create();
+            pageData.Author = null;
+
+            string template = $"{placeholderText}*******{placeholderText}******\r\n****{placeholderText}*********\t\t****{placeholderText}*****{placeholderText}************{placeholderText}";
+            var target = (null as IPageGenerator).Create(template, string.Empty);
+
+            var actual = target.GenerateContentPage(pageData);
+            Console.WriteLine(actual);
+
+            int actualCount = actual.Select((c, i) => actual.Substring(i)).Count(sub => sub.StartsWith(placeholderText));
+            Assert.Equal(0, actualCount);
         }
     }
 }
