7cd9b1df7ea041fbc19166a1816c215d8e654c38
bsstahl <barry@bsstahl.com>
2016-09-08 21:56:56 -0700

    Initial solution setup

---
+++ PrehensilePonyTail/PPTail.Data.FileSystem.Test/Repository_Ctor_Should.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace PPTail.Data.FileSystem.Test
+{
+    public class Repository_Ctor_Should
+    {
+
+        [Fact]
+        public void NotThrowAnException() 
+        {
+            var target = new Repository();
+        }
+
+    }
+}
---
+++ PrehensilePonyTail/PPTail.Data.FileSystem/Repository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PPTail.Data.FileSystem
+{
+    public class Repository
+    {
+        public Repository()
+        {
+        }
+    }
+}
