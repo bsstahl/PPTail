d44e1ac6d5b2adade269cd35c5ae0bf7840dfd10
Barry Stahl <barry@bsstahl.com>
2016-09-09 15:17:19 -0700

    Add tests PageGenerator_GenerateContentPage_Should.ReplaceAContentPlaceholderWithTheContent and PageGenerator_GenerateContentPage_Should.ReplaceAllContentPlaceholdersWithTheContent - Red

--- PrehensilePonyTail/PPTail.Generator.T4Html.Test/PageGenerator_GenerateContentPage_Should.cs
+++ PrehensilePonyTail/PPTail.Generator.T4Html.Test/PageGenerator_GenerateContentPage_Should.cs
@@ -37,5 +37,39 @@ namespace PPTail.Generator.T4Html.Test
             int actualCount = actual.Select((c, i) => actual.Substring(i)).Count(sub => sub.StartsWith(pageData.Title));
             Assert.Equal(5, actualCount);
         }
+
+        [Fact]
+        public void ReplaceAContentPlaceholderWithTheContent()
+        {
+            const string placeholderText = "{Content}";
+
+            var pageData = (null as ContentItem).Create();
+            var expectedData = pageData.Content;
+
+            string template = $"*******************************{placeholderText}*******************************";
+            var target = (null as IPageGenerator).Create(template, string.Empty);
+
+            var actual = target.GenerateContentPage(pageData);
+            Console.WriteLine(actual);
+            Assert.Contains(expectedData, actual);
+        }
+
+        [Fact]
+        public void ReplaceAllContentPlaceholdersWithTheContent()
+        {
+            const string placeholderText = "{Content}";
+
+            var pageData = (null as ContentItem).Create();
+            var expectedData = pageData.Content;
+
+            string template = $"{placeholderText}*******{placeholderText}******\r\n****{placeholderText}*********\t\t****{placeholderText}*****{placeholderText}************{placeholderText}";
+            var target = (null as IPageGenerator).Create(template, string.Empty);
+
+            var actual = target.GenerateContentPage(pageData);
+            Console.WriteLine(actual);
+
+            int actualCount = actual.Select((c, i) => actual.Substring(i)).Count(sub => sub.StartsWith(expectedData));
+            Assert.Equal(6, actualCount);
+        }
     }
 }
