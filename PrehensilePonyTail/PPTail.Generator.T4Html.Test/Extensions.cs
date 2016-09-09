3ac3cbf6eaea47a06491679a957ddfe097ea389f
Barry Stahl <barry@bsstahl.com>
2016-09-09 11:19:51 -0700

    PageGenerator_GenerateContentPage_Should.SetTheContentTitleAsTitleOfThePage - Red

--- PrehensilePonyTail/PPTail.Generator.T4Html.Test/Extensions.cs
+++ PrehensilePonyTail/PPTail.Generator.T4Html.Test/Extensions.cs
@@ -1,8 +1,10 @@
-﻿using PPTail.Interfaces;
+﻿using PPTail.Entities;
+using PPTail.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TestHelperExtensions;
 
 namespace PPTail.Generator.T4Html.Test
 {
@@ -11,6 +13,23 @@ namespace PPTail.Generator.T4Html.Test
         public static IPageGenerator Create(this IPageGenerator ignore)
         {
             return new PPTail.Generator.T4Html.PageGenerator();
+        }
+
+        public static ContentItem Create(this ContentItem ignore)
+        {
+            return new ContentItem()
+            {
+                Author = string.Empty.GetRandom(),
+                CategoryIds = new List<Guid>() { Guid.NewGuid() },
+                Content = string.Empty.GetRandom(),
+                Description = string.Empty.GetRandom(),
+                IsPublished = true,
+                LastModificationDate = DateTime.UtcNow.AddDays(-10.GetRandom()),
+                PublicationDate = DateTime.UtcNow.AddDays(-20.GetRandom(10)),
+                Slug = string.Empty.GetRandom(),
+                Tags = new List<string>() { string.Empty.GetRandom() },
+                Title = string.Empty.GetRandom()
+            };
         }
     }
 }
---
+++ PrehensilePonyTail/PPTail.Generator.T4Html.Test/PageGenerator_GenerateContentPage_Should.cs
@@ -0,0 +1,24 @@
+﻿using PPTail.Entities;
+using PPTail.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PPTail.Generator.T4Html.Test
+{
+    public class PageGenerator_GenerateContentPage_Should
+    {
+        [Fact]
+        public void SetTheContentTitleAsTitleOfThePage()
+        {
+            var target = (null as IPageGenerator).Create();
+            var pageData = (null as ContentItem).Create();
+
+            string expected = $"<title>{pageData.Title}</title>";
+            var actual = target.GenerateContentPage(pageData);
+            Assert.Contains(expected, actual);
+        }
+    }
+}
