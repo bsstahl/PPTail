129b2e1c1ac1c3b8b3965868e5b4ce434c3072b6
Barry Stahl <barry@bsstahl.com>
2016-09-09 15:08:13 -0700

    Add test PageGenerator_GenerateContentPage_Should.ReplaceAllTitlePlaceholdersWithTheTitle.

--- PrehensilePonyTail/PPTail.Generator.T4Html.Test/PageGenerator_GenerateContentPage_Should.cs
+++ PrehensilePonyTail/PPTail.Generator.T4Html.Test/PageGenerator_GenerateContentPage_Should.cs
@@ -22,5 +22,20 @@ namespace PPTail.Generator.T4Html.Test
             Console.WriteLine(actual);
             Assert.Contains(pageData.Title, actual);
         }
+
+        [Fact]
+        public void ReplaceAllTitlePlaceholdersWithTheTitle()
+        {
+            var pageData = (null as ContentItem).Create();
+
+            string template = "{Title}***\r\n************{Title}*********************\t\t****{Title}*****{Title}************{Title}";
+            var target = (null as IPageGenerator).Create(template, string.Empty);
+
+            var actual = target.GenerateContentPage(pageData);
+            Console.WriteLine(actual);
+
+            int actualCount = actual.Select((c, i) => actual.Substring(i)).Count(sub => sub.StartsWith(pageData.Title));
+            Assert.Equal(5, actualCount);
+        }
     }
 }
