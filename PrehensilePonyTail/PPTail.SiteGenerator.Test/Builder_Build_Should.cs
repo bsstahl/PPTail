4f61327c14d1517c1860fadc9f7cfff7ab8e4bec
Barry Stahl <barry@bsstahl.com>
2016-09-09 17:17:38 -0700

    ReturnOneItemInFolderPostsIfOnePostIsRetrieved - Red

--- PrehensilePonyTail/PPTail.SiteGenerator.Test/Builder_Build_Should.cs
+++ PrehensilePonyTail/PPTail.SiteGenerator.Test/Builder_Build_Should.cs
@@ -46,5 +46,17 @@ namespace PPTail.SiteGenerator.Test
             var actual = target.Build();
             Assert.Equal(1, actual.Count(f => f.RelativeFilePath.Contains("\\Pages\\")));
         }
+
+        [Fact]
+        public void ReturnOneItemInFolderPostsIfOnePostIsRetrieved()
+        {
+            var pageGen = Mock.Of<IPageGenerator>();
+            var contentRepo = new Mock<IContentRepository>();
+            var contentItem = (null as ContentItem).Create();
+            contentRepo.Setup(c => c.GetAllPosts()).Returns(() => new List<ContentItem>() { contentItem });
+            var target = (null as Builder).Create(contentRepo.Object, pageGen);
+            var actual = target.Build();
+            Assert.Equal(1, actual.Count(f => f.RelativeFilePath.Contains("\\Posts\\")));
+        }
     }
 }
