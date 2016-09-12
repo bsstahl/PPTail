345ebb0dcfda7ef41ed339b9bfb9e5646e154a8c
bsstahl <barry@bsstahl.com>
2016-09-12 14:16:38 -0700

    SkipPagesWithTheWrongRootNode and SkipPagesWithInvalidSchema - Green

--- PrehensilePonyTail/PPTail.Data.FileSystem/StringExtensions.cs
+++ PrehensilePonyTail/PPTail.Data.FileSystem/StringExtensions.cs
@@ -3,14 +3,46 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace PPTail.Data.FileSystem
 {
     public static class StringExtensions
     {
-        public static ContentItem ParseContentItem(this string data, string rootNode)
+        public static ContentItem ParseContentItem(this string xmlText, string nodeLocalName)
         {
-            return new ContentItem();
+            ContentItem result = null;
+
+            XElement node;
+            try
+            {
+                node = XElement.Parse(xmlText);
+            }
+            catch (System.Xml.XmlException)
+            {
+                node = null;
+            }
+
+            if (node != null && node.Name.LocalName == nodeLocalName)
+            {
+                // string author = node.GetElementValue("author");
+
+                // DateTime publicationDate = DateTime.MinValue;
+                // DateTime.TryParse(node.GetElementValue("pubDate"), out publicationDate);
+
+                result = new ContentItem()
+                {
+                    //IsPublished = Convert.ToBoolean(node.GetElementValue("ispublished")),
+                    //Title = node.GetElementValue("title"),
+                    //Content = node.GetElementValue("content"),
+                    //Slug = node.GetElementValue("slug"),
+                    //Author = author,
+                    //PublicationDate = publicationDate,
+                    //ByLine = string.IsNullOrEmpty(author) ? string.Empty : $"by {author}"
+                };
+            }
+
+            return result;
         }
     }
 }
