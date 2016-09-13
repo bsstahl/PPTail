784052714c2a487b272a8867e014c2e95fdeff81
bsstahl <barry@bsstahl.com>
2016-09-13 08:31:15 -0700

    ReturnTheProperValueInTheContentField - Green

--- PrehensilePonyTail/PPTail.Data.FileSystem/StringExtensions.cs
+++ PrehensilePonyTail/PPTail.Data.FileSystem/StringExtensions.cs
@@ -35,7 +35,7 @@ namespace PPTail.Data.FileSystem
                     //IsPublished = Convert.ToBoolean(node.GetElementValue("ispublished")),
                     Title = node.GetElementValue("title"),
                     Description = node.GetElementValue("description"),
-                    //Content = node.GetElementValue("content"),
+                    Content = node.GetElementValue("content"),
                     //Slug = node.GetElementValue("slug"),
                     Author = author
                     //PublicationDate = publicationDate,
