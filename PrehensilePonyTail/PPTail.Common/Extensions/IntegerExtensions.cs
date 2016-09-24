3079e68f5603ca491dfeec4b1458c54928eca319
bsstahl <barry@bsstahl.com>
2016-09-24 13:57:11 -0400

    Add StdDev function to calculate the population standard deviation of a list of integers. This will be used in the Tag Cloud provider.

---
+++ PrehensilePonyTail/PPTail.Common/Extensions/IntegerExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PPTail.Extensions
+{
+    public static class IntegerExtensions
+    {
+        public static double StdDev(this IEnumerable<int> values)
+        {
+            return 0.0;
+        }
+    }
+}
