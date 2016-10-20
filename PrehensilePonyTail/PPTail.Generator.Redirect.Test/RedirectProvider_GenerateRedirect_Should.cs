1b59636b431a72c60bfef265e9478b0a25ebf79e
Barry Stahl <barry@bsstahl.com>
2016-10-20 15:26:22 -0700

    Implement an IRedirectProvider - Red

---
+++ PrehensilePonyTail/PPTail.Generator.Redirect.Test/RedirectProvider_GenerateRedirect_Should.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestHelperExtensions;
+using Xunit;
+
+namespace PPTail.Generator.Redirect.Test
+{
+    public class RedirectProvider_GenerateRedirect_Should
+    {
+        [Fact]
+        public void IncludeTheUrlInTheOutput()
+        {
+            string url = string.Empty.GetRandom();
+            var target = new RedirectProvider();
+            var actual = target.GenerateRedirect(url);
+            Assert.Contains(url, actual);
+        }
+
+        [Fact]
+        public void IncludeARedirectInTheOutput()
+        {
+            string url = string.Empty.GetRandom();
+            string command = "window.location.assign(";
+            var target = new RedirectProvider();
+            var actual = target.GenerateRedirect(url);
+            Assert.Contains(command, actual);
+        }
+    }
+}
---
+++ PrehensilePonyTail/PPTail.Generator.Redirect/RedirectProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PPTail.Entities;
+using PPTail.Interfaces;
+
+namespace PPTail.Generator.Redirect
+{
+    public class RedirectProvider : IRedirectProvider
+    {
+        public string GenerateRedirect(string redirectToUrl)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
