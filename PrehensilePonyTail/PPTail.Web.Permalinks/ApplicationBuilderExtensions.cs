66def9ff99843a3a8c438bb7eeb3e148f9a764db
bsstahl <barry@bsstahl.com>
2016-11-13 19:35:56 -0700

    Add middleware to handle DasBlog compatibility for syndication and permalinks.

---
+++ PrehensilePonyTail/PPTail.Web.Permalinks/ApplicationBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseDasBlogPermalinks(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<PPTail.Web.Permalinks.DasBlogCompatibility>();
+        }
+    }
+
+
+}
---
+++ PrehensilePonyTail/PPTail.Web.Syndication.Test/DasBlogCompatibility_Ctor_Should.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PPTail.Web.Syndication.Test
+{
+    public class DasBlogCompatibility_Ctor_Should
+    {
+        [Fact]
+        public void ThrowAnArgumentNullExceptionIfTheNextDelegateIsNotSupplied()
+        {
+            var serviceProvider = Mock.Of<IServiceProvider>();
+            Assert.Throws<ArgumentNullException>(() => new DasBlogCompatibility(null));
+        }
+
+        [Fact]
+        public void ReturnTheProperArgumentNameIfTheNextDelegateIsNotSupplied()
+        {
+            string actual = string.Empty;
+            try
+            {
+                var serviceProvider = Mock.Of<IServiceProvider>();
+                var target = new DasBlogCompatibility(null);
+            }
+            catch (ArgumentNullException ex)
+            {
+                actual = ex.ParamName;
+            }
+
+            string expected = "next";
+            Assert.Equal(expected, actual);
+        }
+
+
+    }
+}
