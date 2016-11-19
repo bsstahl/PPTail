91afb9ee19072436a77b8713033c92e27f4e98a0
bsstahl <barry@bsstahl.com>
2016-11-18 19:43:42 -0700

    Add tests for Middleware extension methods

---
+++ PrehensilePonyTail/PPTail.Web.Syndication.Test/ApplicationBuilderExtensions_UseDasBlogPermalinks_Should.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PPTail.Web.Syndication.Test
+{
+    public class ApplicationBuilderExtensions_UseDasBlogPermalinks_Should
+    {
+        [Fact]
+        public void CallUseOnTheApplicationBuilder()
+        {
+            var target = new Mock<IApplicationBuilder>();
+            target.Object.UseDasBlogSyndication();
+            target.Verify(t => t.Use(It.IsAny<Func<RequestDelegate, RequestDelegate>>()), Times.Once);
+        }
+
+        [Fact]
+        public void ReturnTheResultOfTheUseMethod()
+        {
+            var target = new Mock<IApplicationBuilder>();
+            var expected = Mock.Of<IApplicationBuilder>();
+            target.Setup(t => t.Use(It.IsAny<Func<RequestDelegate, RequestDelegate>>()))
+                .Returns(expected);
+            var actual = target.Object.UseDasBlogSyndication();
+            Assert.Equal(expected, actual);
+        }
+    }
+}
