ba839a0cf34b618963db521aa693e114becec0dd
bsstahl <barry@bsstahl.com>
2017-12-16 14:10:09 -0700

    Created projects for wordpress data source

---
+++ PrehensilePonyTail/PPTail.Data.FileSystem.Wordpress.Test/Repository_Ctor_Should.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PPTail.Interfaces;
+using System;
+using Xunit;
+using TestHelperExtensions;
+using Moq;
+using PPTail.Entities;
+using PPTail.Exceptions;
+
+namespace PPTail.Data.FileSystem.Wordpress.Test
+{
+    public class Repository_Ctor_Should
+    {
+        const string _sourceDataPathSettingName = "sourceDataPath";
+
+        [Fact]
+        public void NotThrowAnExceptionIfAllDependenciesAreSupplied()
+        {
+            var settings = new Settings();
+            settings.ExtendedSettings.Set(_sourceDataPathSettingName, string.Empty.GetRandom());
+
+            var fileSystem = Mock.Of<IFile>();
+
+            var container = new ServiceCollection();
+            container.AddSingleton<ISettings>(settings);
+            container.AddSingleton<IFile>(fileSystem);
+
+            var target = new Repository(container.BuildServiceProvider());
+        }
+
+        [Fact]
+        public void ThrowADependencyNotFoundExceptionIfSettingsAreNotProvided()
+        {
+            var fileSystem = Mock.Of<IFile>();
+
+            var container = new ServiceCollection();
+            container.AddSingleton<IFile>(fileSystem);
+
+            Assert.Throws<Exceptions.DependencyNotFoundException>(() => new Repository(container.BuildServiceProvider()));
+        }
+
+        [Fact]
+        public void ThrowWithProperInterfaceTypeNameIfSettingsAreNotProvided()
+        {
+            var fileSystem = Mock.Of<IFile>();
+
+            var container = new ServiceCollection();
+            container.AddSingleton<IFile>(fileSystem);
+
+            string expected = typeof(ISettings).Name;
+            try
+            {
+                var target = new Repository(container.BuildServiceProvider());
+            }
+            catch (DependencyNotFoundException ex)
+            {
+                Assert.Equal(expected, ex.InterfaceTypeName);
+            }
+        }
+
+        [Fact]
+        public void ThrowADependencyNotFoundExceptionIfAFileSystemIsNotProvided()
+        {
+            var settings = new Settings();
+            settings.ExtendedSettings.Set(_sourceDataPathSettingName, string.Empty.GetRandom());
+
+            var container = new ServiceCollection();
+            container.AddSingleton<ISettings>(settings);
+
+            Assert.Throws<Exceptions.DependencyNotFoundException>(() => new Repository(container.BuildServiceProvider()));
+        }
+
+        [Fact]
+        public void ThrowWithProperInterfaceTypeNameIfFileSystemIsNotProvided()
+        {
+            var settings = new Settings();
+            settings.ExtendedSettings.Set(_sourceDataPathSettingName, string.Empty.GetRandom());
+
+            var container = new ServiceCollection();
+            container.AddSingleton<ISettings>(settings);
+
+            string expected = typeof(IFile).Name;
+            try
+            {
+                var target = new Repository(container.BuildServiceProvider());
+            }
+            catch (DependencyNotFoundException ex)
+            {
+                Assert.Equal(expected, ex.InterfaceTypeName);
+            }
+        }
+
+        [Fact]
+        public void ThrowASettingNotFoundExceptionIfTheSourceDataPathIsNotSpecified()
+        {
+            var settings = new Settings();
+            var fileSystem = Mock.Of<IFile>();
+
+            var container = new ServiceCollection();
+            container.AddSingleton<ISettings>(settings);
+            container.AddSingleton<IFile>(fileSystem);
+
+            Assert.Throws<Exceptions.SettingNotFoundException>(() => new Repository(container.BuildServiceProvider()));
+        }
+
+        [Fact]
+        public void ThrowWithProperSettingNameIfTheSourceDataPathIsNotSpecified()
+        {
+            var settings = new Settings();
+            var fileSystem = Mock.Of<IFile>();
+
+            var container = new ServiceCollection();
+            container.AddSingleton<ISettings>(settings);
+            container.AddSingleton<IFile>(fileSystem);
+
+            string expected = _sourceDataPathSettingName;
+            try
+            {
+                var target = new Repository(container.BuildServiceProvider());
+            }
+            catch (SettingNotFoundException ex)
+            {
+                Assert.Equal(expected, ex.SettingName);
+            }
+        }
+    }
+}
---
+++ PrehensilePonyTail/PPTail.Data.FileSystem.Wordpress/Repository.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PPTail.Entities;
+using Microsoft.Extensions.DependencyInjection;
+using System.Xml.Linq;
+using PPTail.Interfaces;
+using PPTail.Extensions;
+
+namespace PPTail.Data.FileSystem.Wordpress
+{
+    public class Repository : Interfaces.IContentRepository
+    {
+        //const int _defaultPostsPerPage = 3;
+        //const int _defaultPostsPerFeed = 5;
+
+        //const string _widgetRelativePath = "datastore\\widgets";
+        //const string _categoriesRelativePath = "categories.xml";
+
+
+        const string _sourceDataPathSettingName = "sourceDataPath";
+
+        private readonly IServiceProvider _serviceProvider;
+        private readonly string _rootDataPath;
+        private readonly string _rootSitePath;
+
+
+        public Repository(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+
+            _serviceProvider.ValidateService<ISettings>();
+            _serviceProvider.ValidateService<IFile>();
+
+            var settings = _serviceProvider.GetService<ISettings>();
+            settings.Validate(_sourceDataPathSettingName);
+
+            _rootSitePath = settings.ExtendedSettings.Get(_sourceDataPathSettingName);
+            _rootDataPath = System.IO.Path.Combine(_rootSitePath, "App_Data");
+        }
+
+        public IEnumerable<ContentItem> GetAllPages()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ContentItem> GetAllPosts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Widget> GetAllWidgets()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<SourceFile> GetFolderContents(string relativePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SiteSettings GetSiteSettings()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
