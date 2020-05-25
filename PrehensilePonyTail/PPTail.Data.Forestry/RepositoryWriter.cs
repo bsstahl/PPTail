d77c9dab5e265df70d96f8611fe3978bf75fbe8c
Barry Stahl <bss_github@bsstahl.com>
2020-05-25 07:24:06 -0700

    Stub Forestry.io writer so I can convert from other formats into that one

---
+++ PrehensilePonyTail/PPTail.Data.Forestry/RepositoryWriter.cs
@@ -0,0 +1,27 @@
+﻿using PPTail.Entities;
+using PPTail.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PPTail.Data.Forestry
+{
+    public class RepositoryWriter : IContentRepositoryWriter
+    {
+        public void SaveAllPages(IEnumerable<ContentItem> pages)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveAllPosts(IEnumerable<ContentItem> posts)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void SaveAllWidgets(IEnumerable<Widget> widgets) => throw new NotImplementedException();
+        public void SaveCategories(IEnumerable<Category> categories) => throw new NotImplementedException();
+        public void SaveFolderContents(String relativePath, IEnumerable<SourceFile> contents) => throw new NotImplementedException();
+        public void SaveSiteSettings(SiteSettings settings) => throw new NotImplementedException();
+    }
+}
